44e1c846dbc6c71072f66a972c18eea8e368ae7a
bencouser <neb2010@live.co.uk>
2023-11-09 17:35:20 +0000

    Lighting of player decreases with change in sanity

--- Assets/Scripts/PlayerSanityMeterUI.cs
+++ Assets/Scripts/PlayerSanityMeterUI.cs
@@ -6,9 +6,10 @@ using UnityEngine.Rendering.Universal;
 
 public class PlayerSanityMeterUI : MonoBehaviour
 {
+
     [SerializeField] private Light2D sanityLight;
     [SerializeField] private PlayerSanityMeter playerSanityMeter;
-/* 
+
     private void Start() {
         if (playerSanityMeter != null) {
             playerSanityMeter.OnSanityChanged += SanityMeter_OnSanityChanged;
@@ -16,13 +17,10 @@ public class PlayerSanityMeterUI : MonoBehaviour
     }
 
     private void SanityMeter_OnSanityChanged(object sender, EventArgs e) {
-        sanityLight.intensity = e.sanityMeterNormalized;
-    }
-
-    private void SanityMeter_OnSanityChanged(object sender, EventArgs e) {
         if (e is PlayerSanityMeter.OnSanityChangedEventArgs args) {
             // Assuming you want the intensity to be proportional to the sanity meter.
             sanityLight.intensity = args.sanityMeterNormalized;
+            Debug.Log(sanityLight.intensity);
         }
     }
 
@@ -30,6 +28,6 @@ public class PlayerSanityMeterUI : MonoBehaviour
         if (playerSanityMeter != null) {
             playerSanityMeter.OnSanityChanged -= SanityMeter_OnSanityChanged;
         }
-    } */
+    }
 }
 
