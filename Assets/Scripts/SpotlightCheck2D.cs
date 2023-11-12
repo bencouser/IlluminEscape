37e77d6f13e1e378efaa2b80b73f77b0afe7ee42
bencouser <neb2010@live.co.uk>
2023-11-12 11:14:56 +0000

    Lamp visuals added, had some problems with making changes not to prefabs but fixes should now work

--- Assets/Scripts/SpotlightCheck2D.cs
+++ Assets/Scripts/SpotlightCheck2D.cs
@@ -7,9 +7,9 @@ using UnityEngine;
 public class SpotlightCheck2D : MonoBehaviour {
     
     public event EventHandler OnIllumination;
-    [SerializeField] private LayerMask obstacleMask; // Using Default Layer Mask
+    [SerializeField] private LayerMask obstacleMask; // Using Floor Layer Mask
     [SerializeField] private GameObject player;
-    [SerializeField] private float spotAngle = 110;  // Set the spotlight angle in degrees
+    [SerializeField] private float spotAngle = 50;  // Set the spotlight angle in degrees
     private float checkRate = 1f;  // Rate at which to check for illumination (checks per second)
     private float nextCheck;
 
@@ -33,22 +33,22 @@ public class SpotlightCheck2D : MonoBehaviour {
                     OnIllumination?.Invoke(this, EventArgs.Empty);
                 }
                 else {
-                    //Debug.Log("Player is not illuminated (obstacle in the way)");
+                    Debug.Log("Player is not illuminated (obstacle in the way)");
                 }
             }
             else {
-                //Debug.Log("Player is not illuminated");
+                Debug.Log("Player is not illuminated");
             }
         } else {
-            //Debug.Log("Player Not in Spotlight Angle");
+            Debug.Log("Player Not in Spotlight Angle");
         }
 
         // Draw rays indicating the spotlight angle
-/*         float halfAngle = spotAngle / 2;
+        float halfAngle = spotAngle / 2;
         Vector3 rightBoundaryDirection = Quaternion.Euler(0, 0, halfAngle) * -transform.up;
         Vector3 leftBoundaryDirection = Quaternion.Euler(0, 0, -halfAngle) * -transform.up;
 
         Debug.DrawRay(transform.position, rightBoundaryDirection * 10, Color.yellow);  // Adjust length as needed
-        Debug.DrawRay(transform.position, leftBoundaryDirection * 10, Color.yellow);  // Adjust length as needed */
+        Debug.DrawRay(transform.position, leftBoundaryDirection * 10, Color.yellow);  // Adjust length as needed
     }
 }
