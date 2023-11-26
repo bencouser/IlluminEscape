e01475488058a16b41fd5af0ae8d737e5e31c666
bencouser <neb2010@live.co.uk>
2023-11-26 17:19:27 +0000

    new assets

---
+++ Assets/Scripts/MiniGameSelector.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniGameSelector
+{
+    static void Main()
+    {
+        // Create a list of strings
+        List<string> items = new List<string> { "Apple", "Banana", "Cherry", "Date", "Elderberry" };
+
+        // Create a random object
+        System.Random random = new System.Random();
+
+        // Choose a random string from the list
+        string randomItem = items[random.Next(items.Count)];
+
+        // Display the chosen string
+        Debug.Log("Randomly selected item: " + randomItem);
+    }
+}
+
